4c1f85fcc464d7f2acbd2cdc0e43e9f011ad0d59
steelysam <steelysam@gmail.com>
2022-09-22 15:12:19 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace DmTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.cmbLocation = new System.Windows.Forms.ComboBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtRandoChecker = new System.Windows.Forms.TextBox();
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.radWeather = new System.Windows.Forms.RadioButton();
+            this.radEncounter = new System.Windows.Forms.RadioButton();
+            this.radLoot = new System.Windows.Forms.RadioButton();
+            this.radNpc = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRoll.Location = new System.Drawing.Point(436, 218);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(77, 40);
+            this.btnRoll.TabIndex = 0;
+            this.btnRoll.Text = "Roll";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            this.btnRoll.Click += new System.EventHandler(this.btnRoll_Click);
+            // 
+            // cmbLocation
+            // 
+            this.cmbLocation.FormattingEnabled = true;
+            this.cmbLocation.Items.AddRange(new object[] {
+            "Arctic",
+            "Coastal",
+            "Desert",
+            "Forest",
+            "Grassland",
+            "Hill",
+            "Mountain",
+            "Swamp",
+            "Underdark",
+            "Underwater",
+            "Urban"});
+            this.cmbLocation.Location = new System.Drawing.Point(36, 105);
+            this.cmbLocation.Name = "cmbLocation";
+            this.cmbLocation.Size = new System.Drawing.Size(121, 21);
+            this.cmbLocation.TabIndex = 1;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(36, 12);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(477, 87);
+            this.txtResult.TabIndex = 2;
+            this.txtResult.Text = "Result text will display here bitch...";
+            // 
+            // txtRandoChecker
+            // 
+            this.txtRandoChecker.Location = new System.Drawing.Point(413, 105);
+            this.txtRandoChecker.Name = "txtRandoChecker";
+            this.txtRandoChecker.Size = new System.Drawing.Size(100, 20);
+            this.txtRandoChecker.TabIndex = 4;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(336, 218);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(77, 40);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // radWeather
+            // 
+            this.radWeather.AutoSize = true;
+            this.radWeather.Location = new System.Drawing.Point(36, 144);
+            this.radWeather.Name = "radWeather";
+            this.radWeather.Size = new System.Drawing.Size(66, 17);
+            this.radWeather.TabIndex = 9;
+            this.radWeather.TabStop = true;
+            this.radWeather.Text = "Weather";
+            this.radWeather.UseVisualStyleBackColor = true;
+            // 
+            // radEncounter
+            // 
+            this.radEncounter.AutoSize = true;
+            this.radEncounter.Location = new System.Drawing.Point(36, 167);
+            this.radEncounter.Name = "radEncounter";
+            this.radEncounter.Size = new System.Drawing.Size(74, 17);
+            this.radEncounter.TabIndex = 10;
+            this.radEncounter.TabStop = true;
+            this.radEncounter.Text = "Encounter";
+            this.radEncounter.UseVisualStyleBackColor = true;
+            // 
+            // radLoot
+            // 
+            this.radLoot.AutoSize = true;
+            this.radLoot.Location = new System.Drawing.Point(36, 190);
+            this.radLoot.Name = "radLoot";
+            this.radLoot.Size = new System.Drawing.Size(46, 17);
+            this.radLoot.TabIndex = 11;
+            this.radLoot.TabStop = true;
+            this.radLoot.Text = "Loot";
+            this.radLoot.UseVisualStyleBackColor = true;
+            // 
+            // radNpc
+            // 
+            this.radNpc.AutoSize = true;
+            this.radNpc.Location = new System.Drawing.Point(36, 213);
+            this.radNpc.Name = "radNpc";
+            this.radNpc.Size = new System.Drawing.Size(47, 17);
+            this.radNpc.TabIndex = 12;
+            this.radNpc.TabStop = true;
+            this.radNpc.Text = "NPC";
+            this.radNpc.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 285);
+            this.Controls.Add(this.radNpc);
+            this.Controls.Add(this.radLoot);
+            this.Controls.Add(this.radEncounter);
+            this.Controls.Add(this.radWeather);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtRandoChecker);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.cmbLocation);
+            this.Controls.Add(this.btnRoll);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.ComboBox cmbLocation;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtRandoChecker;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.RadioButton radWeather;
+        private System.Windows.Forms.RadioButton radEncounter;
+        private System.Windows.Forms.RadioButton radLoot;
+        private System.Windows.Forms.RadioButton radNpc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DmTool
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            cmbLocation.SelectedIndex = 10;
+        }
+
+        private void btnRoll_Click(object sender, EventArgs e)
+        {
+            if(radWeather.Checked)
+            {
+                GenerateTemperature();
+                GenerateWind();
+                GeneratePrecipitation();
+            }
+
+            if(radEncounter.Checked)
+            {
+                GenerateEncounter();
+            }
+
+            if(radLoot.Checked)
+            {
+                GenerateLoot();
+            }
+
+            if(radNpc.Checked)
+            {
+                GenerateNpc();
+            }
+        }
+
+        public void GenerateTemperature()
+        {
+            int randTemp = rnd.Next(20) + 1;
+            int randDegree = rnd.Next(4) + 1;
+            int randFahrenheit = randDegree * 10;
+            int randCelcius = randDegree * 5;
+
+            string normTemp = "Normal for the season.";
+            string hotTemp = randFahrenheit.ToString() + "F/" + randCelcius + "C" + " degrees hotter than normal.";
+            string coldTemp = randFahrenheit.ToString() + "F/" + randCelcius + "C" + " degrees colder than normal.";
+
+            if(randTemp > 14 && randTemp < 18)
+            {
+                txtResult.Text = "Temperature: " + coldTemp + Environment.NewLine;
+            }
+            else if(randTemp >= 18)
+            {
+                txtResult.Text = "Temperature: " + hotTemp + Environment.NewLine;
+            }
+            else
+            {
+                txtResult.Text = "Temperature: " + normTemp + Environment.NewLine;
+            }
+        }
+
+        public void GenerateWind()
+        {
+            int randWind = rnd.Next(20) + 1;
+            string normWind = "no wind";
+            string strongWind = "strong wind";
+            string lightWind = "light wind";
+
+            if (randWind > 12 && randWind < 18)
+            {
+                txtResult.AppendText("Wind: " + lightWind + Environment.NewLine);
+            }
+            else if (randWind >= 18)
+            {
+                txtResult.AppendText("Wind: " + strongWind + Environment.NewLine);
+            }
+            else
+            {
+                txtResult.AppendText("Wind: " + normWind + Environment.NewLine);
+            }
+        }
+
+        public void GeneratePrecipitation()
+        {
+            int randPrecip = rnd.Next(20) + 1;
+            string normPrecip = "no rain or snowfall";
+            string lightPrecip = "light rain or snowfall";
+            string heavyPrecip = "heavy rain or snowfall";
+
+            if (randPrecip > 12 && randPrecip < 18)
+            {
+                txtResult.AppendText("Precipitation: " + lightPrecip + Environment.NewLine);
+            }
+            else if (randPrecip >= 18)
+            {
+                txtResult.AppendText("Precipitation: " + heavyPrecip + Environment.NewLine);
+            }
+            else
+            {
+                txtResult.AppendText("Precipitation: " + normPrecip + Environment.NewLine);
+            }
+        }
+
+        public void GenerateEncounter()
+        {
+            // lol nope...
+            txtResult.Text = "here is an encounter...";
+        }
+
+        public void GenerateLoot()
+        {
+            // generate some loot...
+            txtResult.Text = "here is some loot...";
+        }
+
+        public void GenerateNpc()
+        {
+            // generate an npc...
+            txtResult.Text = "here is an NPC...";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = "";
+        }
+    }
+}
